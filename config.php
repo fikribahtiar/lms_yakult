260984e9c5ed445edd1bd1f5476013f6aedc4aa6
fikribahtiar <muhamad.fikri118@gmail.com>
2022-02-04 15:22:34 +0700

    Menambah Folder Http_Public

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'db_yakult';
+$CFG->dbuser    = 'root';
+$CFG->dbpass    = 'root';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8_general_ci',
+);
+
+$CFG->wwwroot   = 'http://3356-36-77-121-252.ngrok.io';
+$CFG->dataroot  = 'C:\\xampp\htdocs\yakult.diconic.co.id\yakult-moodledata';
+$CFG->admin     = 'admin';
+$CFG->theme		= 'alpha';
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
